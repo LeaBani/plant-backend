d861121ee9550ca2da2b90ba9fbb1b5b2e9afa3c
LeaBani <113905453+LeaBani@users.noreply.github.com>
2023-05-24 00:48:05 +0200

    first commit - project init

---
+++ README.md
@@ -0,0 +1,10 @@
+# MY PLANT
+
+## ABOUT
+
+This project is made with Prisma and TypeScript.
+
+## TEAM
+
+Florine Ferreira
+Lea Bani
---
+++ prisma/migrations/20230523223140_init/migration.sql
@@ -0,0 +1,47 @@
+-- CreateTable
+CREATE TABLE "Plant" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "description" TEXT,
+    "quantity" INTEGER NOT NULL,
+    "picture" TEXT,
+    "category_id" INTEGER NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Plant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Condition" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "Condition_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "firstname" TEXT,
+    "lastname" TEXT,
+    "city" TEXT,
+    "password" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
